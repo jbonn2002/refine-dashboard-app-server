f4728082c4c42a533fdbf02d11842aa14103a55f
Jordy <jbonn2002@gmail.com>
2023-05-18 16:49:42 -0700

    first commit, added initial code for backend

---
+++ controllers/user.controller.js
@@ -0,0 +1,25 @@
+import user from "../mongodb/models/user.js";
+
+const getAllUsers = async (req, res) => {};
+const createUser = async (req, res) => {
+  try {
+    const { name, email, avatar } = req.body;
+
+    const userExists = await user.findOne({ email });
+
+    if (userExists) return res.stats(200).json(userExists);
+
+    const newUser = await user.create({
+      name,
+      email,
+      avatar,
+    });
+
+    res.status(200).json(newUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+const getUserInfoByID = async (req, res) => {};
+
+export { getAllUsers, createUser, getUserInfoByID };
