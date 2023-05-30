13c63a91e73280d944c432bfde7f8beba95ae6c6
Jordy <jbonn2002@gmail.com>
2023-05-30 15:14:11 -0700

    added logic to user controller for get all users

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -29,6 +29,14 @@ const createUser = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
-const getUserInfoByID = async (req, res) => {};
+
+const getUserInfoByID = async (req, res) => {
+  const { id } = req.params;
+  const User = await user.findOne({ _id: id }).populate("allProperties");
+
+  if (User) res.status(200).json(User);
+
+  res.status(404).json({ message: "User not found" });
+};
 
 export { getAllUsers, createUser, getUserInfoByID };
